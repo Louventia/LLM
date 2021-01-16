2e041d902ebb63f1f110ccb6192131253cb533af
Nanisemo <77495343+Nanisemo@users.noreply.github.com>
2021-01-16 21:45:31 +0800

    Add files via upload
    bunch of scripts that I have tried coding earlier.

---
+++ Scripts/TransitionController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionController : MonoBehaviour
+{
+    public Animator transition;
+
+    public float trans_Time = 1f;
+
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0)) // need to change, this is for testing purposes.
+        {
+            Load_Next_Scene();
+        }
+        
+        
+    }
+
+    public void Load_Next_Scene()
+    {
+        StartCoroutine(Load_Scene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator Load_Scene(int level_Index)
+    {
+        // Animation:
+        transition.SetTrigger("End");
+
+        // Delay:
+        yield return new WaitForSeconds(trans_Time);
+
+        // Load:
+        SceneManager.LoadScene(level_Index);
+
+    }
+
+
+
+}
