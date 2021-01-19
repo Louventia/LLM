412ccd0c2b833124b992adba823fdea207f9dc70
Nanisemo <77495343+Nanisemo@users.noreply.github.com>
2021-01-19 16:59:47 +0800

    Add files via upload

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public GameObject dialogueUI;
+
+    public void TriggerDialogue()
+    {
+        dialogueUI.SetActive(true);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
